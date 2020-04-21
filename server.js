dbf3945323814f07ef9e9521d8113c2b4ab68995
JRPino <pino3789@gmail.com>
2020-04-20 17:52:11 -0700

    Added in HTML & JSON Routes, also added Express server and started to setup PORT

--- server.js
+++ server.js
@@ -3,3 +3,38 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+// Express Server
+const app = express();
+// Setting up PORT
+const PORT = process.env.PORT || 7000;
+
+// API Routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ROUTES
+//HTML Routes -- GET Requests
+app.get("/notes", req, res => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("*", req, res => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// JSON Route -- GET Request
+app.get("/api/notes", req, res => {
+    res.sendFile(path.join(__dirname, "db/db.json"));
+});
+
