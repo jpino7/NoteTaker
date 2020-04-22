cafaa6e9a7e41cdbec4a51694426c2e6f8c674df
JRPino <pino3789@gmail.com>
2020-04-21 18:29:01 -0700

    Getting Errors

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.use(express.static(path.join(__dirname, "./public")));
 let notesData = [];
 
 // API Routes
-// API GET
+// API call GET
 app.get("/api/notes", function (err, res) {
+    let returnedNotesData = "";
     try {
-        notesData = fs.readFileSync("./db/db.json", "utf8");
+        returnedNotesData = fs.readFileSync("./db/db.json", "utf8");
         console.log("Success JSON file was read!");
-        notesData = JSON.parse(notesData);
 
         // Error Handling
     } catch (err) {
@@ -30,20 +30,49 @@ app.get("/api/notes", function (err, res) {
         console.log(err);
     }
     // Send to browser
-    res.json(notesData);
+    if (returnedNotesData) {
+        res.json(JSON.parse(returnedNotesData));
+    }
 });
 
-// API POST
+// API call POST
 app.post("/api/notes", function (req, res) {
     try {
-        notesData = fs.readFileSync("./db/db.json", "utf8");
-        console.log(notesData);
-        notesData = JSON.parse(notesData);
-
         // Setting New Notes ID
         req.body.id = notesData.length;
+        console.log(notesData);
+        if (!notesData.isArray()) {
+            notesData = JSON.parse(notesData);
+
+        }
         notesData.push(req.body);
         notesData = JSON.stringify(notesData);
+        // Write New Note to File
+        fs.writeFile("./db/db.json", notesData, "utf8", err => {
+            if (err)
+                throw err;
+        });
+
+        res.json(notesData);
+        // Error Handling
+    } catch (err) {
+        throw err;
+    }
+});
+
+// API call DELETE
+app.delete("/api/notes/:id", function (req, res) {
+    try {
+        notesData = fs.readFileSync("./db/db.json", "utf8");
+        notesData = JSON.parse(notesData);
+
+        // Deletes Old Notes from Array
+        notesData = notesData.filter(note => {
+            return note.id != req.params.id;
+        });
+
+        notesData = JSON.stringify(notesData);
+
         // Write New Note to File
         fs.writeFile("./db/db.json", notesData, "utf8", err => {
             if (err)
@@ -57,31 +86,18 @@ app.post("/api/notes", function (req, res) {
     }
 });
 
-// API DELETE
-
-
-
-
-
-
-
-
-
-
-
-
 // ROUTES
 //HTML Routes -- GET Requests
-app.get("/notes", req, res => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", req, res => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // JSON Route -- GET Request
-app.get("/api/notes", req, res => {
+app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
